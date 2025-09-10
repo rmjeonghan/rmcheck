c7d6790c98acfc201540c8e54ff6918bc8ef34fe
yongs-rm <yongs@rulemakers.co.kr>
2025-09-10 16:59:18 +0900

    로딩스피너 원본 복구

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,28 +1,28 @@
 // src/components/LoadingSpinner.tsx
 "use client";
-// import { motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 
-// const LoadingSpinner = () => {
-//   return (
-//     <div className="flex items-center justify-center min-h-screen bg-slate-50">
-//       <motion.div
-//         style={{
-//           width: 50,
-//           height: 50,
-//           border: '5px solid #e9e9e9',
-//           borderTop: '5px solid #3498db',
-//           borderRadius: '50%',
-//         }}
-//         animate={{ rotate: 360 }}
-//         transition={{
-//           repeat: Infinity,
-//           ease: 'linear',
-//           duration: 1,
-//         }}
-//       />
-//     </div>
-//   );
-// };
+const LoadingSpinner = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-slate-50">
+      <motion.div
+        style={{
+          width: 50,
+          height: 50,
+          border: '5px solid #e9e9e9',
+          borderTop: '5px solid #3498db',
+          borderRadius: '50%',
+        }}
+        animate={{ rotate: 360 }}
+        transition={{
+          repeat: Infinity,
+          ease: 'linear',
+          duration: 1,
+        }}
+      />
+    </div>
+  );
+};
 
 // const LoadingSpinner = () => {
 //   return (
@@ -33,9 +33,9 @@
 
 // };
 
-const LoadingSpinner = () => {
-  return <p>Loading...</p>;
+// const LoadingSpinner = () => {
+//   return <p>Loading...</p>;
 
-};
+// };
 
 export default LoadingSpinner;
