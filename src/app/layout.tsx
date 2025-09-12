b5c151854900f2e01f31da0527afbca7b83594ee
이정한 <jh200210@rulemakers.co.kr>
2025-09-12 12:09:12 +0900

    다크모드 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" suppressHydrationWarning>
       <body className={`${inter.className} bg-slate-50 text-slate-800`}>
         <Providers>{children}</Providers>
       </body>
